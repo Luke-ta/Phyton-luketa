9e8ef9b7a56ee00623e7874f93634da6f367b736
Luke-ta <l.sarmento0542@gmail.com>
2024-12-05 09:48:45 -0300

    Add arquivo Aula 2

---
+++ AULA 2/AULA02.py
@@ -0,0 +1,18 @@
+print ('MESSI MELHOR DO MUNDO')
+
+nome_usuario = 'LUCAS FELIPE'  # tipo --> str = string
+idade_usuario = 110  # tipo --> int = integer
+altura_usuario = 1.75  # tipo --> float = ponto flutuante
+maior_idade = True # tipo --> bool = boolean
+
+print('SEJA BEM VINDO' , nome_usuario )
+print('SUA IDADE É', idade_usuario,'ANOS')
+print('VOCE TEM',altura_usuario,'METROS.')
+print('VOCE É AMIOR DE IDADE?',maior_idade)
+
+
+
+
+
+
+
